163588b246385e992ed310563c5abec9582e6bad
AW63926 <aw63926@gmail.com>
2019-06-29 16:14:30 -0400

    initial commit

---
+++ src/main/java/com/learningSites/LearningSitesApplication.java
@@ -0,0 +1,13 @@
+package com.learningSites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSitesApplication.class, args);
+	}
+
+}
