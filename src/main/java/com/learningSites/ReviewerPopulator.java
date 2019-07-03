6647b9727aa0d80a4f3d6e0415a3240f47cfb8ed
AW63926 <aw63926@gmail.com>
2019-07-03 19:30:40 -0400

    added images for profile pics

--- src/main/java/com/learningSites/ReviewerPopulator.java
+++ src/main/java/com/learningSites/ReviewerPopulator.java
@@ -29,13 +29,13 @@ public class ReviewerPopulator implements CommandLineRunner {
 		Website w3school = new Website("W3schools", "W3logo_200.png");
 		w3school = websiteRepo.save(w3school);
 		
-		Reviewer adam = new Reviewer ("Adam","Cost of learning", "profile.jpg", udemy);
+		Reviewer adam = new Reviewer ("Adam","As a software developer in process, I am always looking for learning resources.  I have ample experience with each of the learning websites we have listed, and I am gathering more experience by the minute!!", "profile.jpg", udemy);
 		adam = reviewerRepo.save(adam);
 		
-		Reviewer mary = new Reviewer ("Mary","Cost of learning", "/img/lynda_logo_200.png", udemy);
+		Reviewer mary = new Reviewer ("Mary","Cost of learning", "mary.JPG", udemy);
 		mary = reviewerRepo.save(mary);
 		
-		Reviewer grace = new Reviewer ("Grace","Cost of learning","/img/lynda_logo_200.png", udemy);
+		Reviewer grace = new Reviewer ("Grace","Cost of learning","grace.JPG", udemy);
 		grace = reviewerRepo.save(grace);
 		
 		reviewRepo.save(new Review("Cost of Learning", grace));
