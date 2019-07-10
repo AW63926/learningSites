590809078aff9491f40dcfaa98604c5263cb88e9
GraceNjoroge <gkmbogo@gmail.com>
2019-07-10 17:16:33 -0400

    AJAX HttpRequests with JS for show-reviewers

--- src/main/java/com/learningSites/Website.java
+++ src/main/java/com/learningSites/Website.java
@@ -18,13 +18,13 @@ public class Website {
 	private Long id;
 
 	private String name;
-	
+
 	private String review;
-	
+
 	private String starRating;
-	
+
 	private String starRating2;
-	
+
 	private String starRating3;
 
 	@ManyToMany(mappedBy = "websites")
@@ -40,41 +40,16 @@ public class Website {
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getImage() {
 		return websiteImage;
 	}
 
 	public Website() {
 	}
-	
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Website other = (Website) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	public Website(String name, String webImage, String review, String starRating, String starRating2, String starRating3) {
+	public Website(String name, String webImage, String review, String starRating, String starRating2,
+			String starRating3) {
 		this.name = name;
 		this.websiteImage = webImage;
 		this.review = review;
@@ -83,15 +58,15 @@ public class Website {
 		this.starRating3 = starRating3;
 
 	}
-	
+
 	public Website(String name, String review, String starRating, String starRating2, String starRating3) {
-	this.name = name;
-	this.websiteImage = "user.png";
-	this.review = review;
-	this.starRating = starRating;
-	this.starRating2 = starRating2;
-	this.starRating3 = starRating3;
-	
+		this.name = name;
+		this.websiteImage = "user.png";
+		this.review = review;
+		this.starRating = starRating;
+		this.starRating2 = starRating2;
+		this.starRating3 = starRating3;
+
 	}
 
 	public String getReview() {
@@ -128,6 +103,31 @@ public class Website {
 			urls.add(format("/show-websites/%d/reviewers/%s", this.getId(), r.getName()));
 		}
 		return urls;
-	}	
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Website other = (Website) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
